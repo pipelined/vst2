8c6241baed38f02b84b35661a9ce4720f4868668
Sergei <sergdudk@sergdudk.local>
2017-12-22 08:06:24 +0100

    Rename bridge to callback

--- vst2.go
+++ vst2.go
@@ -1,15 +1,46 @@
 package vst2
 
 /*
+#cgo CFLAGS: -std=gnu99 -I${SRCDIR}/vst2sdk/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "aeffectx.h"
 
+// Plugin's entry point
 typedef AEffect * (*vstPluginFuncPtr)(audioMasterCallback host);
 
-AEffect * loadEffect(AEffect * (*load)(vstPluginFuncPtr));
+//Go callback prototype
+int hostCallback(AEffect *effect, int opcode, int index, int value, void *ptr, float opt);
 
-double** processAudio(AEffect *effect, int numChannels, int blocksize, double ** goInputs);
+//Bridge function to call entry point on AEffect
+AEffect * loadEffect(AEffect * (*load)(audioMasterCallback)){
+	return load((audioMasterCallback)hostCallback);
+}
 
-int dispatch(AEffect *effect, int opcode, int index, int value, void *ptr, float opt);
+//Bridge to call dispatch function of loaded plugin
+VstIntPtr dispatch(AEffect *effect, int opcode, int index, int value, void *ptr, float opt){
+	return effect->dispatcher(effect, opcode, index, value, ptr, opt);
+}
+
+//Bridge to call process replacing function of loaded plugin
+double** processAudio(AEffect *effect, int numChannels, int blocksize, double ** goInputs){
+
+	double** inputs = (double**)malloc(sizeof(double**) * numChannels);
+	for(int channel = 0; channel < numChannels; channel++) {
+    	inputs[channel] = (double*)&goInputs[channel];
+  	}
+
+	double** outputs = (double**)malloc(sizeof(double**) * numChannels);
+	for(int channel = 0; channel < numChannels; channel++) {
+    	outputs[channel] = (double*)malloc(sizeof(double*) * blocksize);
+  	}
+
+	effect -> processDoubleReplacing(effect, inputs, outputs, blocksize);
+
+	return outputs;
+}
 */
 import "C"
 
@@ -121,16 +152,6 @@ func (plugin *Plugin) Process(samples [][]float64) (processed [][]float64) {
 		processed[channel] = (*[1 << 30]float64)(unsafe.Pointer(data))[:blocksize]
 	}
 	return processed
-}
-
-//export hostCallback
-//calls real callback
-func hostCallback(effect *C.AEffect, opcode int64, index int64, value int64, ptr unsafe.Pointer, opt float64) int {
-	if callback == nil {
-		panic("Host callback is not defined!")
-	}
-
-	return callback(&Plugin{effect: effect}, MasterOpcode(opcode), index, value, ptr, opt)
 }
 
 //SetHostCallback allows to override default host callback with custom implementation

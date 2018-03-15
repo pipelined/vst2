697a0885ec7f0ae26f788b31926bf46b56a98a8a
dudk <serg.dudk@gmail.com>
2018-03-15 14:22:40 +0100

    Simplify the plugin tests

--- vst2_darwin_test.go
+++
@@ -1,6 +0,0 @@
-package vst2
-
-const (
-	// pluginPath = "/Library/Audio/Plug-ins/VST/Krush.vst"
-	pluginPath = "_testdata/Krush.vst"
-)
--- vst2_windows_test.go
+++
@@ -1,5 +0,0 @@
-package vst2
-
-const (
-	pluginPath = "_testdata/2-band distortion.dll"
-)

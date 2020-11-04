52b76ef60c3f2d2c5e2a817c2f0440b650691cba
Sergei Dudka <serg.dudk@gmail.com>
2020-11-04 22:15:20 +0100

    rename effect to plugin

--- sdk.c
+++ sdk.c
@@ -4,34 +4,34 @@
 #include "sdk.h"
 
 //Go callback prototype
-int hostCallback(Effect *effect, int32_t opcode, int32_t index, int64_t value, void *ptr, float opt);
+int hostCallback(Plugin *plugin, int32_t opcode, int32_t index, int64_t value, void *ptr, float opt);
 
 //Bridge function to call entry point on Effect
-Effect * loadEffect(EntryPoint load){
+Plugin * loadPlugin(EntryPoint load){
 	return load((HostCallback)hostCallback);
 }
 
 // Bridge to call dispatch function of loaded plugin
-int64_t dispatch(Effect *effect, int32_t opcode, int32_t index, int64_t value, void *ptr, float opt){
-	return effect->dispatcher(effect, opcode, index, value, ptr, opt);
+int64_t dispatch(Plugin *plugin, int32_t opcode, int32_t index, int64_t value, void *ptr, float opt){
+	return plugin->dispatcher(plugin, opcode, index, value, ptr, opt);
 }
 
 // Bridge to call process replacing function of loaded plugin
-void processDouble(Effect *effect, int32_t numChannels, int32_t blocksize, double ** inputs, double ** outputs){
-	effect -> processDoubleReplacing(effect, inputs, outputs, blocksize);
+void processDouble(Plugin *plugin, int32_t numChannels, int32_t blocksize, double ** inputs, double ** outputs){
+	plugin -> processDoubleReplacing(plugin, inputs, outputs, blocksize);
 }
 
 // Bridge to call process replacing function of loaded plugin
-void processFloat(Effect *effect, int32_t numChannels, int32_t blocksize, float **inputs, float **outputs){
-	effect -> processReplacing(effect, inputs, outputs, blocksize);
+void processFloat(Plugin *plugin, int32_t numChannels, int32_t blocksize, float **inputs, float **outputs){
+	plugin -> processReplacing(plugin, inputs, outputs, blocksize);
 }
 
 // Bridge to call get parameter fucntion of loaded plugin
-float getParameter(Effect *effect, int32_t paramIndex) {
-	return effect->getParameter(effect, paramIndex);
+float getParameter(Plugin *plugin, int32_t paramIndex) {
+	return plugin->getParameter(plugin, paramIndex);
 }
 
 // Bridge to call set parameter fucntion of loaded plugin
-void setParameter(Effect *effect, int32_t paramIndex, float value) {
-	effect->setParameter(effect, paramIndex, value);
+void setParameter(Plugin *plugin, int32_t paramIndex, float value) {
+	plugin->setParameter(plugin, paramIndex, value);
 }
--- vst.go
+++ vst.go
@@ -8,7 +8,7 @@ import "C"
 import "unsafe"
 
 const (
-	// VST main function name.
+	// VST main function namp.
 	main = "VSTPluginMain"
 	// VST API version.
 	version = 2400
@@ -16,38 +16,38 @@ const (
 
 type (
 	// VST is a reference to VST main function.
-	// It also keeps reference to VST handle to clean up on Close.
+	// It also keeps reference to VST handle to clean up on Closp.
 	VST struct {
-		main   effectMain
+		main   pluginMain
 		handle uintptr
 		Name   string
 	}
 
-	// Effect is an instance of loaded VST plugin.
-	Effect C.Effect
+	// Plugin is an instance of loaded VST plugin.
+	Plugin C.Plugin
 
-	// effectMain is a reference to VST main function.
+	// pluginMain is a reference to VST main function.
 	// wrapper on C entry point.
-	effectMain C.EntryPoint
+	pluginMain C.EntryPoint
 )
 
 // Plugin new instance of VST plugin with provided callback.
-// This function also calls dispatch with EffOpen opcode.
-func (v *VST) Plugin(c HostCallbackFunc) *Effect {
+// This function also calls dispatch with EffOpen opcodp.
+func (v *VST) Plugin(c HostCallbackFunc) *Plugin {
 	if v.main == nil || c == nil {
 		return nil
 	}
-	e := (*Effect)(C.loadEffect(v.main))
+	p := (*Plugin)(C.loadPlugin(v.main))
 	mutex.Lock()
-	callbacks[e] = c
+	callbacks[p] = c
 	mutex.Unlock()
 
-	return e
+	return p
 }
 
 // Dispatch wraps-up C method to dispatch calls to plugin
-func (e *Effect) Dispatch(opcode EffectOpcode, index Index, value Value, ptr unsafe.Pointer, opt Opt) Return {
-	return Return(C.dispatch((*C.Effect)(e), C.int32_t(opcode), C.int32_t(index), C.int64_t(value), unsafe.Pointer(ptr), C.float(opt)))
+func (p *Plugin) Dispatch(opcode PluginOpcode, index Index, value Value, ptr unsafe.Pointer, opt Opt) Return {
+	return Return(C.dispatch((*C.Plugin)(p), C.int32_t(opcode), C.int32_t(index), C.int64_t(value), unsafe.Pointer(ptr), C.float(opt)))
 }
 
 // ScanPaths returns a slice of default vst2 locations.
@@ -59,24 +59,24 @@ func ScanPaths() []string {
 }
 
 // NumParams returns the number of parameters.
-func (e *Effect) NumParams() int {
-	return int(e.numParams)
+func (p *Plugin) NumParams() int {
+	return int(p.numParams)
 }
 
 // NumPrograms returns the number of programs.
-func (e *Effect) NumPrograms() int {
-	return int(e.numPrograms)
+func (p *Plugin) NumPrograms() int {
+	return int(p.numPrograms)
 }
 
-// Flags returns the effect flags.
-func (e *Effect) Flags() EffectFlags {
-	return EffectFlags(e.flags)
+// Flags returns the plugin flags.
+func (p *Plugin) Flags() PluginFlags {
+	return PluginFlags(p.flags)
 }
 
 // ProcessDouble audio with VST plugin.
-func (e *Effect) ProcessDouble(in, out DoubleBuffer) {
+func (p *Plugin) ProcessDouble(in, out DoubleBuffer) {
 	C.processDouble(
-		(*C.Effect)(e),
+		(*C.Plugin)(p),
 		C.int32_t(in.numChannels),
 		C.int32_t(in.size),
 		&in.data[0],
@@ -85,9 +85,9 @@ func (e *Effect) ProcessDouble(in, out DoubleBuffer) {
 }
 
 // ProcessFloat audio with VST plugin.
-func (e *Effect) ProcessFloat(in, out FloatBuffer) {
+func (p *Plugin) ProcessFloat(in, out FloatBuffer) {
 	C.processFloat(
-		(*C.Effect)(e),
+		(*C.Plugin)(p),
 		C.int32_t(in.numChannels),
 		C.int32_t(in.size),
 		&in.data[0],
@@ -96,21 +96,21 @@ func (e *Effect) ProcessFloat(in, out FloatBuffer) {
 }
 
 // ParamValue returns the value of parameter.
-func (e *Effect) ParamValue(index int) float32 {
-	return float32(C.getParameter((*C.Effect)(e), C.int32_t(index)))
+func (p *Plugin) ParamValue(index int) float32 {
+	return float32(C.getParameter((*C.Plugin)(p), C.int32_t(index)))
 }
 
 // SetParamValue sets new value for parameter.
-func (e *Effect) SetParamValue(index int, value float32) {
-	C.setParameter((*C.Effect)(e), C.int32_t(index), C.float(value))
+func (p *Plugin) SetParamValue(index int, value float32) {
+	C.setParameter((*C.Plugin)(p), C.int32_t(index), C.float(value))
 }
 
 // CanProcessFloat32 checks if plugin can process float32.
-func (e *Effect) CanProcessFloat32() bool {
-	return EffectFlags(e.flags)&EffFlagsCanReplacing == EffFlagsCanReplacing
+func (p *Plugin) CanProcessFloat32() bool {
+	return PluginFlags(p.flags)&PluginFloatProcessing == PluginFloatProcessing
 }
 
 // CanProcessFloat64 checks if plugin can process float64.
-func (e *Effect) CanProcessFloat64() bool {
-	return EffectFlags(e.flags)&EffFlagsCanDoubleReplacing == EffFlagsCanDoubleReplacing
+func (p *Plugin) CanProcessFloat64() bool {
+	return PluginFlags(p.flags)&PluginDoubleProcessing == PluginDoubleProcessing
 }

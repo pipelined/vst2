f2be7cd38ab82852c4c9bb274e45bd8d99583bcb
Sergei Dudka <serg.dudk@gmail.com>
2020-11-04 21:00:43 +0100

    fix godoc vet

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// package vst2 provides all types available in VST2 SDK. The purpose of it
+// Package vst2 provides all types available in VST2 SDK. The purpose of it
 // is to enable to develop vst2 aplications without a need to interact with
 // CGO and unsafe directly.
 package vst2

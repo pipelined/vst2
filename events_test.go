30855dce90b254ae35cff9c4f5d317f4097efb86
Sergei Dudka <serg.dudk@gmail.com>
2020-11-16 23:56:40 +0100

    add get event

--- events_test.go
+++ events_test.go
@@ -1,6 +1,7 @@
 package vst2_test
 
 import (
+	"fmt"
 	"testing"
 
 	"pipelined.dev/audio/vst2"
@@ -10,6 +11,7 @@ func TestEvents(t *testing.T) {
 	dump := vst2.SysExData([]byte("this is a test"))
 	defer dump.Free()
 	events := vst2.Events(
+		&vst2.MIDIEvent{},
 		&vst2.SysExMIDIEvent{
 			SysExDump: dump,
 		},
@@ -18,4 +20,13 @@ func TestEvents(t *testing.T) {
 
 	assertNotNil(t, "events", events)
 	vst2.TestEvents(events)
+
+	for i := 0; i < events.NumEvents(); i++ {
+		fmt.Printf("%v", events.Event(i))
+		switch ev := events.Event(i).(type) {
+		case *vst2.MIDIEvent:
+		case *vst2.SysExMIDIEvent:
+			fmt.Printf("dump %v\n", ev.SysExDump)
+		}
+	}
 }

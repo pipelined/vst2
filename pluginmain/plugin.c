41ad8b94bdc2fa105c08a9a4114b4107b06b3468
Sergei Dudka <serg.dudk@gmail.com>
2021-03-14 13:46:54 +0100

    remove obsolete code

--- pluginmain/plugin.c
+++ pluginmain/plugin.c
@@ -26,31 +26,3 @@ Plugin* VSTPluginMain(HostCallback c) {
     p->processFloat = processFloat;
     return p;
 }
-
-// // Bridge to call dispatch function of loaded plugin
-// int64_t dispatchPluginBridge(Plugin *plugin, int32_t opcode, int32_t index, int64_t value, void *ptr, float opt){
-// 	return dispatch(plugin, opcode, index, value, ptr, opt);
-// }
-
-// // Bridge to call process replacing function of loaded plugin
-// void processDoublePluginBridge(Plugin *plugin, double ** inputs, double ** outputs, int32_t sampleFrames){
-// 	processDouble(plugin, inputs, outputs, sampleFrames);
-// }
-
-
-// // Bridge to call process replacing function of loaded plugin
-// void processFloatPluginBridge(Plugin *plugin, float **inputs, float **outputs, int32_t sampleFrames){
-// 	processFloat(plugin, inputs, outputs, sampleFrames);
-// }
-
-
-// // Bridge to call get parameter fucntion of loaded plugin
-// float getParameterPluginBridge(Plugin *plugin, int32_t paramIndex) {
-// 	getParameter(plugin, paramIndex);
-// }
-
-
-// // Bridge to call set parameter fucntion of loaded plugin
-// void setParameterPluginBridge(Plugin *plugin, int32_t paramIndex, float value) {
-// 	setParameter(plugin, paramIndex, value);
-// }

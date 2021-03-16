61af527bb05a83eed53908afafcf4d221438246b
Sergei Dudka <serg.dudk@gmail.com>
2021-03-16 20:24:13 +0100

    move plugin to vst2, make go plugin struct

---
+++ plugin.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "plugin.h"
+
+//Go dispatch prototype
+int64_t dispatchPluginBridge(CPlugin *plugin, int32_t opcode, int32_t index, int64_t value, void *ptr, float opt);
+
+//Go processDouble prototype
+void processDoublePluginBridge(CPlugin *plugin, double ** inputs, double ** outputs, int32_t sampleFrames);
+
+//Go processFloat prototype
+void processFloatPluginBridge(CPlugin *plugin, float **inputs, float **outputs, int32_t sampleFrames);
+
+//Go getParameter prototype
+float getParameterPluginBridge(CPlugin *plugin, int32_t paramIndex);
+
+//Go setParameter prototype
+void setParameterPluginBridge(CPlugin *plugin, int32_t paramIndex, float value);
+
+CPlugin* VSTPluginMain(HostCallback c) {
+    // TODO: init values from go plugin
+    CPlugin *p = malloc(sizeof(CPlugin));
+    p->dispatcher = dispatchPluginBridge;
+    p->getParameter = getParameterPluginBridge;
+    p->setParameter = setParameterPluginBridge;
+    p->processDouble = processDoublePluginBridge;
+    p->processFloat = processFloatPluginBridge;
+    return p;
+}

9f4a8ef3d8d14e7974271f28bc1dac4ad7c098ff
Sergei Dudka <serg.dudk@gmail.com>
2021-03-29 23:47:56 +0200

    inject host in plugin allocator

--- plugin_export.go
+++ plugin_export.go
@@ -11,7 +11,7 @@ import (
 // instantiate go plugin
 //export newGoPlugin
 func newGoPlugin(cp *C.CPlugin, c C.HostCallback) {
-	p := PluginAllocator(HostCallback{c})
+	p := PluginAllocator(callbackHandler{c}.host())
 	cp.magic = C.int(EffectMagic)
 	cp.numInputs = C.int(p.InputChannels)
 	cp.numOutputs = C.int(p.OutputChannels)
@@ -21,7 +21,7 @@ func newGoPlugin(cp *C.CPlugin, c C.HostCallback) {
 		p.inputDouble = DoubleBuffer{data: make([]*C.double, p.InputChannels)}
 		p.outputDouble = DoubleBuffer{data: make([]*C.double, p.OutputChannels)}
 	}
-	if p.PluginFloatProcessing != nil {
+	if p.ProcessFloatFunc != nil {
 		cp.flags = cp.flags | C.int(PluginFloatProcessing)
 		p.inputFloat = FloatBuffer{data: make([]*C.float, p.InputChannels)}
 		p.outputFloat = FloatBuffer{data: make([]*C.float, p.OutputChannels)}
